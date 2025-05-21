209497d577d49c675b084326233bb836bddee580
Farida594 <faridaapril45@gmail.com>
2025-05-21 21:44:13 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,21 @@
+package com.constructors;
+
+public class Employee {
+	String name; 
+	 double salary; 
+	 public Employee(String name,double salary) { 
+	  this.name=name; 
+	  this.salary=salary; 
+	  if(salary<0) { 
+	   salary=0; 
+	  } 
+	 } 
+	 public void displayDetails() { 
+	  System.out.println("Name:"+name); 
+	  System.out.println("Salary is:"+salary); 
+	 } 
+	 public static void main(String[] args) { 
+	  Employee e=new Employee("Farida",100000); 
+	  e.displayDetails(); 
+	 }
+}
